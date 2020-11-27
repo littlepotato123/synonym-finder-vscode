4f5b2659675d3babe60d7fe43eea0603bb5e7135
littlepotato123 <sopanda0910@gmail.com>
2020-11-26 20:03:11 -0500

    Clearning

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import fetch from 'node-fetch';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "synonym-finder" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+export const activate = (context: vscode.ExtensionContext) => {
 	let disposable = vscode.commands.registerCommand(
 		'synonym-finder.helloWorld', 
 		async () => {
@@ -30,7 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
 			quickPick.items = data.map((x: any) => ({ label: x.word }));
 			quickPick.onDidChangeSelection(([item]) => {
 				if(item) {
-					vscode.window.showInformationMessage(item.label);
 					editor.edit((edit) => {
 						edit.replace(editor.selection, item.label);
 					})
@@ -45,5 +32,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export const deactivate = () => {};
